4272c1c2fcb8e99d62dcec5095b4236cb2df323e
kt <kt@a2ndeiMac.local>
2015-02-01 14:45:24 +0800

    CNY:PHP

--- XSocial/Demo.m
+++ XSocial/Demo.m
@@ -8,6 +8,11 @@
 
 #import "Demo.h"
 #import "SwiftXSocial-swift.h"
+
+
+//CNY:PHP   http://api.k780.com:88/?app=finance.rate&scur=CNY&tcur=PHP&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json
+
+
 @implementation Demo
 - (void)sayHello {
     NSLog(@"dd   heihei");
